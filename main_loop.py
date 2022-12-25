ae6badeb0861391021300569d2affadbfb8942cb
LingHu <michel11.zhao@gmail.com>
2022-12-25 02:49:07 +0100

    re added on join and reaction role

--- main_loop.py
+++ main_loop.py
@@ -27,6 +27,57 @@ class MyClient(discord.Client):
         if message.content.startswith('!'):
             await command_response.main(message)
 
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is None:
+            return
+
+        to_send = \
+            f'Welcome {member.mention} to {guild.name}!\n' + \
+            'Change name to your in game name\n' + \
+            'To apply tag @staff in <#1055160381288501348>\n' + \
+            'check <#1055865903906046054> if you\'re interested in guild events'
+        await guild.system_channel.send(to_send)
+
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent) :
+        if payload.message_id != self.role_message_id :
+            return
+
+        try :
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError :
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        role = guild.get_role(role_id)
+        try :
+            await payload.member.add_roles(role)
+        except discord.HTTPException :
+            pass
+
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        if payload.message_id != self.role_message_id :
+            return
+
+        try :
+            role_id = self.emoji_to_role[payload.emoji]
+        except KeyError :
+            return
+
+        guild = self.get_guild(payload.guild_id)
+        role = guild.get_role(role_id)
+        if role is None :
+            return
+
+        member = guild.get_member(payload.user_id)
+        if member is None :
+            return
+
+        try :
+            await member.remove_roles(role)
+        except discord.HTTPException:
+            pass
+
 
 intents = discord.Intents.default()
 intents.message_content = True
