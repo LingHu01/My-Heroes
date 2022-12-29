514204be5619d064f1e0a740d31433760328e8b6
LingHu <michel11.zhao@gmail.com>
2022-12-29 19:48:17 +0100

    fixed webhook avatar

--- GIF.py
+++ GIF.py
@@ -1,15 +1,9 @@
-import requests
 import discord
 async def post_GIF(key, GIF_dict, channel, message):
     if key in GIF_dict.keys() :
         file ='/'.join(GIF_dict[key].split('\\'))
-        original = message.author.display_name
+        url = message.author.display_avatar
         name = message.author.display_name
-        try :
-            req = requests.get(url=original.avatar.url)
-            avatar = req.content
-            webhook = await channel.create_webhook(name=name, avatar=avatar)
-        except AttributeError :
-            webhook = await channel.create_webhook(name=name)
-            await webhook.send(file=discord.File(file))
+        webhook = await channel.create_webhook(name=name)
+        await webhook.send(file=discord.File(file), avatar_url=url)
         await webhook.delete()
