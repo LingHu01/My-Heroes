8811477c8cf73c5b251cbd51465015a0625a57f3
LingHu <michel11.zhao@gmail.com>
2022-12-29 03:48:15 +0100

    added GIF

---
+++ GIF.py
@@ -0,0 +1,15 @@
+import requests
+import discord
+async def post_GIF(key, GIF_dict, channel, message):
+    if key in GIF_dict.keys() :
+        file = GIF_dict[key]
+        original = message.author.display_name
+        name = message.author.display_name
+        try :
+            req = requests.get(url=original.avatar.url)
+            avatar = req.content
+            webhook = await channel.create_webhook(name=name, avatar=avatar)
+        except AttributeError :
+            webhook = await channel.create_webhook(name=name)
+            await webhook.send(file=discord.File(file))
+        await webhook.delete()
