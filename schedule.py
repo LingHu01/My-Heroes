e2121bc753791b4533a85f1b0ff9817b1869cba0
LingHu <michel11.zhao@gmail.com>
2023-01-17 13:49:18 +0100

    schedule adjustement

--- schedule.py
+++ schedule.py
@@ -6,6 +6,7 @@ from command import bulk_delete
 async def schedule_message(self, channel):
     server_timezone = pytz.timezone("America/New_York")
     while True:
+        await sleep(60)
         time_info = datetime.datetime.now(server_timezone)
         weekday = datetime.datetime.weekday(time_info) + 1
         current_server_time = time_info.strftime("%H:%M")
@@ -13,17 +14,17 @@ async def schedule_message(self, channel):
         hours, minutes = map(int, self.night_raid_time[weekday].split(':'))
         target_time = datetime.time(hours, minutes, 0)
 
+        if weekday not in self.night_raid_time.keys():
+            if current_server_time in {'08:00', '15:00', '20:00'}:
+                await channel.send('<@&1055864488357810357>')
+            continue
+
         if target_time.strftime('%H:%M') == (time_info + datetime.timedelta(minutes= 30)).strftime('%H:%M'):
             await channel.send('<@&1055864399421771807> in 30 min')
         if target_time.strftime('%H:%M') == (time_info + datetime.timedelta(minutes= 5)).strftime('%H:%M'):
             await channel.send('<@&1055864399421771807> in 5 min')
         if target_time.strftime('%H:%M') == time_info.strftime('%H:%M'):
             await channel.send('<@&1055864399421771807> now')
-        if weekday in range(5, 7):
-            if current_server_time in {'08:00', '15:00', '20:00'}:
-                await channel.send('<@&1055864488357810357>')
 
-        if current_server_time == '01:00' and weekday == 6:
+        if current_server_time == '01:00' and weekday == 1:
             await bulk_delete(channel)
-
-        await sleep(60)
