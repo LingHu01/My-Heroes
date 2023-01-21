08bc86992e9b1374ff009c06885a26a0c6698a40
LingHu <michel11.zhao@gmail.com>
2023-01-22 00:49:59 +0100

    fix schedule.py

--- schedule.py
+++ schedule.py
@@ -11,14 +11,13 @@ async def schedule_message(self, channel):
         weekday = datetime.datetime.weekday(time_info) + 1
         current_server_time = time_info.strftime("%H:%M")
 
-        hours, minutes = map(int, self.night_raid_time[weekday].split(':'))
-        target_time = datetime.time(hours, minutes, 0)
-
         if weekday not in self.night_raid_time.keys():
             if current_server_time in {'08:00', '15:00', '20:00'}:
                 await channel.send('<@&1055864488357810357>')
             continue
 
+        hours, minutes = map(int, self.night_raid_time[weekday].split(':'))
+        target_time = datetime.time(hours, minutes, 0)
         if target_time.strftime('%H:%M') == (time_info + datetime.timedelta(minutes= 30)).strftime('%H:%M'):
             await channel.send('<@&1055864399421771807> in 30 min')
         if target_time.strftime('%H:%M') == (time_info + datetime.timedelta(minutes= 5)).strftime('%H:%M'):
