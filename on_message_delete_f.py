acadb32907448cf001084bb843e1aa93d6a6579c
LingHu <michel11.zhao@gmail.com>
2023-03-07 23:13:39 +0100

    log system

--- on_message_delete_f.py
+++ on_message_delete_f.py
@@ -1,6 +1,8 @@
 import discord
 
 async def main(self, message):
-    if not message.author.bot :
-        embed = discord.Embed(title=f'{message.author.name} was deleted', color=0x51F5EA, description= message.content)
-        await self.log_channel.send(embed=embed)
+    if message.author.bot :
+        return
+
+    embed = discord.Embed(title=f'message from {message.author.display_name} was deleted', color=0x51F5EA, description= message.content)
+    await self.log_channel.send(embed=embed)
--- on_message_edit_f.py
+++ on_message_edit_f.py
@@ -2,7 +2,7 @@ import discord
 
 async def main(self, before, after):
 	if before.content != after.content:
-		embed = discord.Embed(title=f'{before.author.name} edited', color=0x51F5EA)
+		embed = discord.Embed(title=f'{before.author.display_name} message edited', color=0x51F5EA)
 		embed.add_field(name= 'from', value= before.content)
 		embed.add_field(name='to', value=after.content)
 		await self.log_channel.send(embed= embed)
