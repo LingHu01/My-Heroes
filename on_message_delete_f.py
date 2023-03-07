6cdc2768fce80f7037d6232a35ee359f408d6089
LingHu <michel11.zhao@gmail.com>
2023-03-07 23:26:47 +0100

    log system

--- on_message_delete_f.py
+++ on_message_delete_f.py
@@ -1,8 +1,7 @@
 import discord
 
 async def main(self, message):
-    if message.author.bot :
+    if message.content.startswith('!'):
         return
-
-    embed = discord.Embed(title=f'message from {message.author.display_name} was deleted', color=0x51F5EA, description= message.content)
+    embed = discord.Embed(title=f'message from {message.author.name} was deleted', color=0x51F5EA, description= message.content)
     await self.log_channel.send(embed=embed)
